11f818e25f6d62b1614809d485c2922fd8741391
Ozan Özten <goldenhawkk@gmail.com>
2021-01-30 20:30:31 +0300

    Controlled Component example

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
 export default class SearchBar extends React.Component {
-    onInputChange(e){
-        console.log(e.target.value)
-    }
+    state = { searchTerm: '' }
 
     render() {
         return (
@@ -12,7 +10,10 @@ export default class SearchBar extends React.Component {
                     <div className="field">
                         <label>Search</label>
                         {/* Be careful not to invoke the event handler methods by putting (). */}
-                        <input type="text" onChange={this.onInputChange} />
+                        <input 
+                            type="text" 
+                            value={this.state.searchTerm} 
+                            onChange={e => this.setState({searchTerm: e.target.value})} />
                     </div>
                     
                     {/* <button className="ui button" type="submit">Submit</button> */}
@@ -32,10 +33,12 @@ export default class SearchBar extends React.Component {
 */
 
 /*
-    The input element is an example of UNCONTROLLED element. 
+    The input element was an example of UNCONTROLLED element. 
 
     Becase we don't know its value. We have to dive into the DOM in order to find out its value. 
     This means the source of truth is inside the HTML document, not inside of our React component.
     
-    In React we don't like to store information inside HTML elements.
+    In React we don't like to store information inside HTML elements.  
+
+    Since we set its value from the state object, React knows its value and this makes it CONTROLLED ELEMENT.
 */
