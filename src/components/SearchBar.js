9c61a46cfa89d28f44d0a23bac16bc8b903f0024
Ozan Özten <goldenhawkk@gmail.com>
2021-01-30 20:14:22 +0300

    Handling events

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 export default class SearchBar extends React.Component {
+    onInputChange(e){
+        console.log(e.target.value)
+    }
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Search</label>
-                        <input type="text"/>
+                        {/* Be careful not to invoke the event handler methods by putting (). */}
+                        <input type="text" onChange={this.onInputChange} />
                     </div>
                     
                     {/* <button className="ui button" type="submit">Submit</button> */}
@@ -16,3 +21,12 @@ export default class SearchBar extends React.Component {
         )
     }
 }
+
+/*
+    Handling events:
+        We can pass a callback function:
+            1) onChange={this.onInputChange}
+        
+        We can pass an arrow func directly to the prop itself:
+            2) onChange={event => console.log(event.target.value)}
+*/
