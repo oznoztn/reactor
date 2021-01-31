c2a6cced4f98b45418c0cb188fb0207b66734d54
Ozan Özten <goldenhawkk@gmail.com>
2021-01-31 16:47:54 +0300

    Using refs, vanilla js event handler attaching, etc.

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
+
   render() {
     const { id, description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <a href={urls.full} target="_blank">
-          <img key={id} alt={description} src={urls.small}  />
+          <img ref={this.imageRef} key={id} alt={description} src={urls.small}  />
         </a>
       </div>
     );
