5de4c031b4f94d212a50270dc9163b11756ae243
Ozan Özten <goldenhawkk@gmail.com>
2021-02-01 21:38:31 +0300

    useEffect Hook example

--- src/components-3/WidgetsApp.js
+++ src/components-3/WidgetsApp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Accordion from './Accordion'
+import SearchWidget from './SearchWidget'
 
 const favGames = [
     {
@@ -25,6 +26,11 @@ class WidgetsApp extends React.Component{
     render(){
         return (
             <div>
+                <SearchWidget/>
+                
+                <br/>
+                <div class="ui horizontal divider"><h2>Accordion Widget</h2></div>
+                
                 <Accordion items={favGames}/>
             </div>
         )
